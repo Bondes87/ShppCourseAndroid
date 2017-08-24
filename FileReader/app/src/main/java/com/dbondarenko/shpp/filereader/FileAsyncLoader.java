a6e5e6dc7ed015b11cbbccf3cb3ffbeceabc73fb
Dmitro Bondarenko <bondes87@gmail.com>
2017-08-24 22:19:09 +0300

    Create FileAsyncLoader

---
+++ FileReader/app/src/main/java/com/dbondarenko/shpp/filereader/FileAsyncLoader.java
@@ -0,0 +1,78 @@
+package com.dbondarenko.shpp.filereader;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.text.TextUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.io.Writer;
+
+/**
+ * File: FileAsyncLoader.java
+ * Created by Dmitro Bondarenko on 24.08.2017.
+ */
+public class FileAsyncLoader extends AsyncTaskLoader<String> {
+    private String fileName;
+
+    public FileAsyncLoader(Context context, String fileName) {
+        super(context);
+        this.fileName = fileName;
+        onContentChanged();
+    }
+
+    @Override
+    public void onStartLoading() {
+        if (takeContentChanged()) {
+            forceLoad();
+        }
+    }
+
+    @Override
+    public String loadInBackground() {
+        AssetManager assetManager = getContext().getAssets();
+        String result = null;
+        if (!TextUtils.isEmpty(fileName)) {
+            InputStream inputStream = null;
+            try {
+                inputStream = assetManager.open(fileName);
+                result = convertStreamToString(inputStream);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (inputStream != null) {
+                        inputStream.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public void onStopLoading() {
+        cancelLoad();
+    }
+
+    private static String convertStreamToString(InputStream inputStream) throws IOException {
+        Writer writer = new StringWriter();
+        char[] buffer = new char[1024];
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+            int charactersNumber;
+            while ((charactersNumber = reader.read(buffer)) != -1) {
+                writer.write(buffer, 0, charactersNumber);
+            }
+        } finally {
+            inputStream.close();
+        }
+        return writer.toString();
+    }
+}
--- FileReader/app/src/main/java/com/dbondarenko/shpp/filereader/MainActivity.java
+++ FileReader/app/src/main/java/com/dbondarenko/shpp/filereader/MainActivity.java
@@ -1,13 +1,32 @@
 package com.dbondarenko.shpp.filereader;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView textViewFileContents;
+    private Button buttonReadFile;
+
+    @Override
+    public void onClick(View v) {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initViews();
         setContentView(R.layout.activity_main);
+    }
+
+    private void initViews() {
+        setContentView(R.layout.activity_main);
+        buttonReadFile = (Button) findViewById(R.id.buttonReadFile);
+        buttonReadFile.setOnClickListener(this);
+        textViewFileContents = (TextView) findViewById(R.id.textViewFileContents);
     }
 }
