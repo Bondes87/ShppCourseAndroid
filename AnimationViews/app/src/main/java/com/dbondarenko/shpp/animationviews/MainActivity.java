84486bbe483134f718d5ba2a25a7d0d4fd475920
Dmitro Bondarenko <bondes87@gmail.com>
2017-09-27 12:55:09 +0300

    3. Used ButterKnife for finding the view and assigning listeners by clicks.

--- AnimationViews/app/src/main/java/com/dbondarenko/shpp/animationviews/MainActivity.java
+++ AnimationViews/app/src/main/java/com/dbondarenko/shpp/animationviews/MainActivity.java
@@ -1,15 +1,49 @@
 package com.dbondarenko.shpp.animationviews;
 
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.OnLongClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.constraintLayoutBoard)
+    ConstraintLayout mConstraintLayoutBoard;
+    @BindView(R.id.textViewStickerA)
+    TextView textViewStickerA;
+    @BindView(R.id.textViewStickerB)
+    TextView textViewStickerB;
+    @BindView(R.id.textViewStickerC)
+    TextView textViewStickerC;
+    @BindView(R.id.textViewStickerD)
+    TextView textViewStickerD;
+    @BindView(R.id.textViewStickerE)
+    TextView textViewStickerE;
+    @BindView(R.id.textViewStickerF)
+    TextView textViewStickerF;
+    @BindView(R.id.colorfulViewSticker)
+    ColorfulView colorfulViewSticker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //ColorfulView colorfulView = (ColorfulView) findViewById(R.id.colorful_view);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.constraintLayoutBoard)
+    public void onViewClicked() {
+        textViewStickerA.setText("0");
+    }
+
+    @OnLongClick(R.id.constraintLayoutBoard)
+    public boolean onViewLongClicked() {
+        textViewStickerA.setText("1");
+        return true;
     }
 }
