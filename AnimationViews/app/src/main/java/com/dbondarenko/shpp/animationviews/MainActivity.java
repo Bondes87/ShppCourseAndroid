5d12f35f8628a0c20d3ddce3f48f45c18df1b458
Dmitro Bondarenko <bondes87@gmail.com>
2017-09-28 14:34:42 +0300

    3. Realized the execution of the animation to the end.

--- AnimationViews/app/src/main/java/com/dbondarenko/shpp/animationviews/MainActivity.java
+++ AnimationViews/app/src/main/java/com/dbondarenko/shpp/animationviews/MainActivity.java
@@ -1,9 +1,12 @@
 package com.dbondarenko.shpp.animationviews;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.view.animation.LinearInterpolator;
 import android.widget.TextView;
 
@@ -20,15 +23,20 @@ public class MainActivity extends AppCompatActivity {
     public static final int ROTATION_Y_FOR_RETURN = -360;
     public static final int TRANSLATION_X_FOR_RETURN = 0;
     public static final int TRANSLATION_Y_FOR_RETURN = 0;
+    public static final String PROPERTY_X = "x";
+    public static final String PROPERTY_Y = "y";
+    public static final String PROPERTY_ROTATION_X = "rotationX";
+    public static final String PROPERTY_ROTATION_Y = "rotationY";
+    public static final String PROPERTY_TRANSLATION_X = "translationX";
+    public static final String PROPERTY_TRANSLATION_Y = "translationY";
     private static final int ONE_SECOND = 1000;
     public static final int DURATION_OF_RETURN_ANIMATION = ONE_SECOND;
     private static final String LOG_TAG = "main_activity";
-
     @BindView(R.id.constraintLayoutBoard)
     ConstraintLayout constraintLayoutBoard;
     @BindView(R.id.textViewStickerA)
     TextView textViewStickerA;
-    @BindView(R.id.textViewStickerB)
+    /*@BindView(R.id.textViewStickerB)
     TextView textViewStickerB;
     @BindView(R.id.textViewStickerC)
     TextView textViewStickerC;
@@ -39,9 +47,10 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.textViewStickerF)
     TextView textViewStickerF;
     @BindView(R.id.colorfulViewSticker)
-    ColorfulView colorfulViewSticker;
+    ColorfulView colorfulViewSticker;*/
 
     private boolean isStickersReturn;
+    private AnimatorSet slowestAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,12 +58,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        slowestAnimation = new AnimatorSet();
     }
 
     @OnClick(R.id.constraintLayoutBoard)
     public void onViewClicked() {
         Log.d(LOG_TAG, "onViewClicked()");
-        if (!isStickersReturn) {
+        if (!isStickersReturn && !slowestAnimation.isRunning()) {
             isStickersReturn = true;
             runOfFallAnimation();
         }
@@ -63,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
     @OnLongClick(R.id.constraintLayoutBoard)
     public boolean onViewLongClicked() {
         Log.d(LOG_TAG, "onViewLongClicked()");
-        if (isStickersReturn) {
+        if (isStickersReturn && !slowestAnimation.isRunning()) {
             isStickersReturn = false;
             runOfReturnAnimation();
         }
@@ -88,26 +98,40 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void runOfFallAnimation() {
+        long maxValueOfDuration = 0;
         for (int i = 0; i < constraintLayoutBoard.getChildCount(); i++) {
-            constraintLayoutBoard.getChildAt(i).animate()
-                    .setDuration(getValueOfDuration())
-                    .setInterpolator(new LinearInterpolator())
-                    .x(getValueOfX())
-                    .y(getValueOfY())
-                    .rotationX(ROTATION_X_FOR_FALL).
-                    rotationY(ROTATION_Y_FOR_FALL);
+            long currentValueOfDuration = getValueOfDuration();
+            View sticker = constraintLayoutBoard.getChildAt(i);
+            AnimatorSet fallAnimation = new AnimatorSet();
+            fallAnimation.setInterpolator(new LinearInterpolator());
+            fallAnimation.setDuration(currentValueOfDuration);
+            fallAnimation.playTogether(ObjectAnimator.ofFloat(sticker, PROPERTY_X, getValueOfX()),
+                    ObjectAnimator.ofFloat(sticker, PROPERTY_Y, getValueOfY()),
+                    ObjectAnimator.ofFloat(sticker, PROPERTY_ROTATION_X, ROTATION_X_FOR_FALL),
+                    ObjectAnimator.ofFloat(sticker, PROPERTY_ROTATION_Y, ROTATION_Y_FOR_FALL));
+            fallAnimation.start();
+            if (currentValueOfDuration > maxValueOfDuration) {
+                maxValueOfDuration = currentValueOfDuration;
+                slowestAnimation = fallAnimation;
+            }
         }
     }
 
     private void runOfReturnAnimation() {
         for (int i = 0; i < constraintLayoutBoard.getChildCount(); i++) {
-            constraintLayoutBoard.getChildAt(i).animate()
-                    .setDuration(DURATION_OF_RETURN_ANIMATION)
-                    .setInterpolator(new LinearInterpolator())
-                    .translationX(TRANSLATION_X_FOR_RETURN)
-                    .translationY(TRANSLATION_Y_FOR_RETURN)
-                    .rotationX(ROTATION_X_FOR_RETURN).
-                    rotationY(ROTATION_Y_FOR_RETURN);
+            View sticker = constraintLayoutBoard.getChildAt(i);
+            AnimatorSet returnAnimation = new AnimatorSet();
+            returnAnimation.setInterpolator(new LinearInterpolator());
+            returnAnimation.setDuration(DURATION_OF_RETURN_ANIMATION);
+            returnAnimation.playTogether(
+                    ObjectAnimator.ofFloat(sticker, PROPERTY_TRANSLATION_X, TRANSLATION_X_FOR_RETURN),
+                    ObjectAnimator.ofFloat(sticker, PROPERTY_TRANSLATION_Y, TRANSLATION_Y_FOR_RETURN),
+                    ObjectAnimator.ofFloat(sticker, PROPERTY_ROTATION_X, ROTATION_X_FOR_RETURN),
+                    ObjectAnimator.ofFloat(sticker, PROPERTY_ROTATION_Y, ROTATION_Y_FOR_RETURN));
+            returnAnimation.start();
+            if (i == constraintLayoutBoard.getChildCount() - 1) {
+                slowestAnimation = returnAnimation;
+            }
         }
     }
 }
