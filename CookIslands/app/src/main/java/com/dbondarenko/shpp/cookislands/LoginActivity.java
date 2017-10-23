5e888098ff0a97c8004f9447725b3564cd204a1e
Dmitro Bondarenko <bondes87@gmail.com>
2017-10-23 16:42:28 +0300

    5. Init project CookIslands.

---
+++ CookIslands/app/src/main/java/com/dbondarenko/shpp/cookislands/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.dbondarenko.shpp.cookislands;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * The login screen that offers login via login / password.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
+
